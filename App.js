0ce185a436981823bc35b00bfe3a3ece48034bd4
Espen Gitlesen <espengi@uia.no>
2023-03-13 23:57:37 +0100

    react native paper theme

--- App.js
+++ App.js
@@ -2,24 +2,36 @@
 
 import { NavigationContainer } from "@react-navigation/native"
 import Dashboard from "./pages/dashboard"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { useColorScheme } from "react-native"
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
+import { Provider, MD3DarkTheme, MD3LightTheme } from "react-native-paper";
 
-
-const Tab = createBottomTabNavigator()
+const Tab = createMaterialBottomTabNavigator()
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard}/>
+      <Tab.Screen 
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: 'map',
+        }}/>
     </Tab.Navigator>
   )
 }
 
 export default function App() {
+  const colorScheme = useColorScheme()
+  const isDarkMode = colorScheme === "dark"
+
+  const theme = isDarkMode ? MD3DarkTheme : MD3LightTheme
   return (
-    <NavigationContainer>
+    <Provider theme={theme}>
+      <NavigationContainer theme={theme}>
         <MyTabs/>
     </NavigationContainer>
+    </Provider>
   )
 }
