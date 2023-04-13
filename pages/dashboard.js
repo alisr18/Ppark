19494daa91232187d0e26b27145e41e8f04508e1
Matin Mohammadi <matinm@student.uia.no>
2023-04-13 19:58:32 +0200

    Reslove merge conflict

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,14 +4,37 @@ import React, {useState} from 'react';
 import { Provider, Text } from "react-native-paper";
 import { View } from "react-native";
 
+import Mapbox from '@rnmapbox/maps';
 
+Mapbox.setAccessToken('pk.eyJ1IjoibWF0aW4xMiIsImEiOiJjbGZibTZxOHMwNGM0M3RreDN0YXN6cjB4In0.LCs5uxPjPMHym58UzqmTWw');
 const Dashboard = ({user}) => {
     return (
-        <View style={{alignItems: "center", flex: 1, marginTop: 50}}>
-            <Text>You are logged in.</Text>
-            <Text>Welcome, {user.email}!</Text>
-        </View>   
+        <View style={styles.page}>
+            <View style={styles.container}>
+                <Mapbox.MapView style={styles.map} />
+            </View>
+        </View>
     );
 }
 
 export default Dashboard;
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        height: 300,
+        width: 300,
+    },
+    map: {
+        flex: 1
+    }
+});
