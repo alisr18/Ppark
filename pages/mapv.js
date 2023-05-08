851751c5915ed9fbd0b1533fe0275c42b8f37de2
Matin Mohammadi <matinm@student.uia.no>
2023-05-08 20:08:27 +0200

    fixed the current button

--- pages/mapv.js
+++ pages/mapv.js
@@ -3,10 +3,11 @@
 import React,{useState, useEffect, useContext} from 'react';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, View, Alert, TouchableOpacity} from 'react-native';
 import { Dimensions } from 'react-native';
 import MapViewDirections from 'react-native-maps-directions';
 import {googleapikey} from '@env'
+import Icon from 'react-native-vector-icons/Ionicons'; //kansje tar det vek
 
 import * as Location from 'expo-location';
 import {useRef} from "react";
@@ -35,7 +36,14 @@ const mapv = () => {
 
     const [origin, setOrigin] = useState({ latitude: 58.3343, longitude: 8.5781 })
     const [destination, setDestination] = useState({ latitude: null, longitude: null })
+    const [SearchRegion, setSearchRegion] = useState(null)
+
+
+
     const mapRef = useRef(null)
+
+
+
 
     useEffect(() => {
         getCurrentLocationPermission()
@@ -61,8 +69,17 @@ const mapv = () => {
         }
         let location = await Location.getCurrentPositionAsync({})
         setOrigin({ latitude: location.coords.latitude, longitude: location.coords.longitude })
-    }
-    function handleDestinationSelect(data, details) {
+
+
+
+        mapRef.current.animateToRegion({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+        });
+           }
+    function handleDestinationSelect(data,details) {
         const destinationLocation = {
             latitude: details.geometry.location.lat,
             longitude: details.geometry.location.lng
@@ -73,18 +90,52 @@ const mapv = () => {
         })
     }
 
+    const handleSerachRegion = (data, details) =>{
+        const region= {
+            longitude: details.geometry.location.lng,
+            latitude: details.geometry.location.lat,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
+
+        }
+        try {
+            setSearchRegion(region);
+
+            console.log("pregion lat:",region.latitude);
+            console.log("pregion lng:",region.longitude);
+            console.log("details; ",details.geometry.location.lng,)
+
+            mapRef.current.animateToRegion(SearchRegion);
+        }catch (error)
+        {
+            Alert.alert("Error","Failed to fetch data. Please try again")
+        }
+
+    }
+
+    const handleSearch = ()=>
+    {
+        console.log("search button has been pressd. ")
+    }
+
+
+
+
+
+
     return (
         <View style={styles.container}>
             <MapView
+                region={SearchRegion}
                 ref={mapRef}
+
                 provider={PROVIDER_GOOGLE}
-                customMapStyle={{
-                    myLocationButtonPosition: 'bottomRight',
-                    theme: theme.dark ? theme.map.dark : theme.map.light
-                }}
+                customMapStyle={
+                    {theme: theme.dark ? theme.map.dark : theme.map.light}
+                }
 
                 showsUserLocation={true}
-                showsMyLocationButton={true}
+                showsMyLocationButton={false}
                 userLocationUpdateInterval={2000}
                 followsUserLocation={true}
                 showsTraffic={true}
@@ -92,19 +143,26 @@ const mapv = () => {
                 initialRegion={{
                     latitude: origin.latitude,
                     longitude: origin.longitude,
-                    latitudeDelta: 0.005,
-                    longitudeDelta: 0.005
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
                 }}
             >
+                {SearchRegion?.location && (
+                    <Marker coordinate={{latitude: SearchRegion.latitude, longtiude :SearchRegion.longitude}}
+                            description="A great city to visit"
+
+                    />)}
+
+
                 {origin?.location && (
-                    <MapView.Marker
+                    <Marker
                         resizeMode="contain"
                         coordinate={{ latitude: origin.latitude, longitude: origin.longitude }}
                         identifier={'origin'}
                     />
                 )}
                 {destination?.location && (
-                    <MapView.Marker
+                    <Marker
                         resizeMode="contain"
                         coordinate={{ latitude: destination.latitude, longitude: destination.longitude }}
                         identifier={'destination'}
@@ -117,7 +175,7 @@ const mapv = () => {
                         origin={{ latitude: origin.latitude, longitude: origin.longitude }}
                         destination={{ latitude: destination.latitude, longitude: destination.longitude }}
                         strokeColor={'black'}
-                        strokeWidth={1}
+                        strokeWidth={2}
                         optimizeWaypoints={true}
                         onStart={(params) => {
                             console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
@@ -126,17 +184,32 @@ const mapv = () => {
                 )}
             </MapView>
 
-            <View style={{ position: 'absolute', width: '80%' }}>
+            <View style={styles.searchContainer} >
                 <GooglePlacesAutocomplete
                     placeholder="Where to park?"
-                    styles={styles.searchbar}
+                    styles={{
+                        textInputContainer: styles.textInputContainer,
+                        textInput: styles.textInput,
+                        listView: styles.listView,
+                    }}
+
+                    renderRightButton={() => (
+                        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+                            <Icon name='search-outline' size={24} color='#333' />
+                        </TouchableOpacity>  )}
+
                     fetchDetails={true}
-                    onPress={handleDestinationSelect}
+                    onPress={handleSerachRegion}
                     query={{
                         key: googleapikey,
                         language: 'en'
                     }}
                 />
+            </View>
+            <View style={styles.currentLocationButton}>
+                <TouchableOpacity onPress={getCurrentLocationPermission}>
+                    <Icon name="my-location" size={28} color="#333" />
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -152,19 +225,59 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+
+
     map: {
         width: '100%',
         height: '100%',
     },
-    searchbar:{
-        borderRadius: 10,
-        margin: 10,
-        color: '#000',
-        borderColor: '#666',
-        backgroundColor: '#FFF',
-        borderWidth: 1,
-        height: 45,
-        paddingHorizontal: 10,
-        fontSize: 18,
-    }
+
+
+    searchContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        margin: 4,
+        overflow: "hidden",
+        backgroundColor: "#fff",
+        borderRadius: 30
+    },
+    textInputContainer: {
+        backgroundColor: '#f5f5f5',
+        borderRadius: 5,
+        borderWidth: 0,
+        height: 50,
+        marginTop: 5
+    },
+    listView: {
+        backgroundColor: '#f5f5f5',
+        borderRadius: 5,
+        borderWidth: 0,
+        marginTop:10,
+    },
+    searchButton: {
+        marginRight: 10,
+        marginTop:10
+    },
+    currentLocationButton: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+        backgroundColor: '#fff',
+        borderRadius: 30,
+        height: 60,
+        width: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+
 });
