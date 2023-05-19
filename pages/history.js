dd64e23f6d906a931bd0d288f793cdc1ac50e5b3
Henninglar <henningl@uia.no>
2023-05-19 06:04:25 +0200

    fixed

--- pages/history.js
+++ pages/history.js
@@ -1,16 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AuthContext } from "../authContext";
 import { IconButton } from "react-native-paper";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {collection, query, where, getDocs, orderBy} from "firebase/firestore";
 import { db } from "../firebaseConfig";
+import { List } from 'react-native-paper';
 
 const History = () => {
     const { user } = useContext(AuthContext);
     const navigate = useNavigation();
     const [data, setData] = useState([]);
-
+    const [dataA, setDataa] = useState([]);
     useEffect(() => {
         fetchHistoryData();
     }, []);
@@ -18,82 +19,122 @@ const History = () => {
     const fetchHistoryData = async () => {
         try {
             const userId = user.uid;
-            const q = query(collection(db, "orders"), where("renter", "==", userId));
-            const snapshot = await getDocs(q);
+            const renterQuery = query(
+                collection(db, "orders"),
+                where("renter", "==", userId),
+                orderBy("endDate", "desc")
+            );
 
-            const fetchedData = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
+            const ownerQuery = query(
+                collection(db, "orders"),
+                where("owner", "==", userId),
+            );
+
+
+            const renterSnapshot = await getDocs(renterQuery);
+            const ownerSnapshot = await getDocs(ownerQuery);
+
+            const fetchedRenterData = renterSnapshot.docs.map((doc) => ({
+                price: doc.data().price,
+                ed: doc.data().endDate.toDate().toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                }),
+                address: doc.data().address,
+                car: doc.data().car,
+                owner: doc.data().owner
             }));
+
+            const fetchedOwnerData = ownerSnapshot.docs.map((doc) => ({
+                price: doc.data().price,
+                ed: doc.data().endDate.toDate().toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                }),
+                address: doc.data().address,
+                car: doc.data().car,
+                owner: doc.data().owner
+            }));
+
+
+            setData(fetchedRenterData);
+            setDataa(fetchedOwnerData);
         } catch (error) {
             console.error("Error fetching history data: ", error);
         }
     };
 
     return (
-        <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
             <View style={styles.header}>
-                <Text style={styles.columnHeader}>Column 1</Text>
-                <Text style={styles.columnHeader}>Column 2</Text>
-                <Text style={styles.columnHeader}>Column 3</Text>
+                <IconButton
+                    style={styles.backButton}
+                    icon="arrow-left"
+                    onPress={navigate.goBack}
+                />
+                <Text style={styles.title}>History</Text>
             </View>
-            <View style={styles.body}>
-                {data.length === 0 ? (
-                    <Text>No history data available</Text>
-                ) : (
-                    data.map((item, index) => (
-                        <View key={index} style={styles.row}>
-                            <Text style={styles.cell}>{item.column1}</Text>
-                            <Text style={styles.cell}>{item.column2}</Text>
-                            <Text style={styles.cell}>{item.column3}</Text>
-                        </View>
-                    ))
-                )}
-            </View>
-            <IconButton
-                style={styles.close}
-                onPress={navigate.goBack}
-                icon="arrow-left"
-                mode="contained-tonal"
-            />
-        </View>
+            {data.length === 0 ? (
+                <Text style={styles.noDataText}>No history data available</Text>
+            ) : (
+                <List.Section>
+                    {data.map((item, index) => (
+                        <List.Item
+                            left={(props) => <List.Icon
+                                icon={item.owner == user.uid? "parking" : "car"}
+                                {...props}
+                            />}
+                            key={index}
+                            title={`Address: ${item.address}`}
+                            description={`Car: ${item.car}, Date Ended: ${item.ed},\n${
+                                item.owner === user.uid ? "Earned: " + item.price + " NOK" : "Paid: " + item.price + " NOK"
+                            }`}
+                        />
+                    ))}
+                </List.Section>
+            )}
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: "center",
-        paddingTop: 20,
+        flexGrow: 1,
+        paddingVertical: 20,
+        paddingHorizontal: 10,
     },
     header: {
         flexDirection: "row",
-        backgroundColor: "lightgray",
-        paddingHorizontal: 10,
-        paddingVertical: 5,
+        alignItems: "center",
         marginBottom: 10,
     },
-    columnHeader: {
-        flex: 1,
+    backButton: {
+        marginRight: 10,
+    },
+    title: {
+        fontSize: 20,
         fontWeight: "bold",
+    },
+    noDataText: {
+        fontSize: 18,
         textAlign: "center",
+        marginTop: 20,
     },
-    body: {
-        paddingHorizontal: 10,
+    listItem: {
+        marginBottom: 10,
+        borderRadius: 8,
+        elevation: 2,
+        backgroundColor: "#f9f9f9",
     },
-    row: {
-        flexDirection: "row",
-        borderBottomWidth: 1,
-        borderBottomColor: "gray",
-        paddingVertical: 5,
+    listItemTitle: {
+        fontSize: 16,
+        fontWeight: "bold",
     },
-    cell: {
-        flex: 1,
-        textAlign: "center",
-    },
-    close: {
-        alignSelf: "flex-start",
-        marginTop: 25,
+    listItemDescription: {
+        fontSize: 14,
+        color: "#888",
     },
 });
 
